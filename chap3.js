7bf1f7fabe6e4bc448963908051dd3bae0e3db84
Kyle Tan <kylebrandontan@gmail.com>
2020-04-09 13:05:44 +0800

    Recursion

--- chap3.js
+++ chap3.js
@@ -7,3 +7,23 @@ console.log("The future says.. ", future());
 function future() {
   return "try!"
 }
+
+
+// A lesson in Recursion
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+			 find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+
+
+console.log(findSolution(13));
+// → (((1 * 3) + 5) * 3)
