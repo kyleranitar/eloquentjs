55346eb681f0b2c22ca70cae22de02ae1d3795b7
Kyle Tan <kylebrandontan@gmail.com>
2020-04-11 10:34:37 +0800

    Groundwork for exercise 3

--- chap4.js
+++ chap4.js
@@ -92,3 +92,29 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+//Exercise 3
+function arrayToList(array) {
+  let list = {};
+  for (let values of array) {
+
+  }
+}
+
+function listToArray(list) {
+
+}
+
+function prepend(element, list) {
+}
+
+function nth(list, number) {
+}
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
