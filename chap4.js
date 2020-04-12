2366ca9046d5255a3dd368758804e2cb973ca000
Kyle Tan <kylebrandontan@gmail.com>
2020-04-12 12:19:02 +0800

    chap 4 ex3 2 functions left

--- chap4.js
+++ chap4.js
@@ -66,7 +66,28 @@ for (let event of journalEvents(JOURNAL)) {
 // Serializing the data -> converting to a flat description
 
 
-// Exercises
+// Exercise 1
+function range(x, y, step = x < y ? 1 : -1) {
+  let array = [];
+  if (step > 0) {
+    for (let i = x; i <= y; i += step) {
+      array.push(i);
+    }
+  } else if (step < 0) {
+    for (let i = x; i >= y; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+}
 
 // Exercise 2
 function reverseArray(array) {
@@ -95,26 +116,25 @@ console.log(arrayValue);
 
 //Exercise 3
 function arrayToList(array) {
-  let list = {};
-  for (let values of array) {
-
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
   }
+  return list;
 }
 
 function listToArray(list) {
-
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
 }
 
-function prepend(element, list) {
-}
 
-function nth(list, number) {
-}
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+function prepend()
+
+let list = {value: 10, rest: {value: 20, rest: null}};
+let arr = [1, 2, 3, 4]
+console.log(arrayToList(arr));
+console.log(listToArray(list));
