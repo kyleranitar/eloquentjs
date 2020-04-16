e46b9d738e9322d287520076c68b3ca8b89f0090
Kyle Tan <kylebrandontan@gmail.com>
2020-04-16 10:04:05 +0800

    Finished chap5

--- chap5.js
+++ chap5.js
@@ -40,3 +40,39 @@ function characterCount(script) {
 }
 
 // Strings and Character Codes
+
+// Exercise 1
+let arrays =[[1,2,3],[4,5],[6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current, [])));
+
+// Exercise 2
+function loop(start, test, update, body) {
+  for (let value = start; test(value); value = update(value)) {
+    body(value);
+  }
+}
+
+// Exercise 3
+function every(array, test) {
+  for (let element of array) {
+    if (!test(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, test) {
+  return !array.some(element => !test(element))
+}
+
+// Exercise 4
+function dominantDirection(text) {
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter({name} => name != "none");
+
+  if(counted.length == 0) return "ltr";
+
+  return counted.reduce((a,b) => a.count > b.count ? a : b).name; 
+}
